6f030cb4d48e3f13bca6f433e362b45169e055e9
Valerio Guerrini <valerio.guerrini@telecom-paris.fr>
2025-05-12 14:31:48 +0200

    add multivariate_distance

--- tsmd/tools/distance.py
+++ tsmd/tools/distance.py
@@ -2,6 +2,7 @@
 Distance are coded to work jointly with the KNN class. They are build as an iterator class. A basic distance shoud be as follow: 
 """
 import numpy as np
+from scipy.signal import fftconvolve
 
 class Basicdistance(object): 
 
@@ -748,3 +749,81 @@ class LTNormalizedEuclidean(object):
         dist = (self.dot_product_ - self.wlen*(self.means_[self.idx_]*self.means_+ self.stdt**2 * self.alphas_[self.idx_]*self.alphas_))/(self.etas_[self.idx_]*self.etas_)
         dist = np.sqrt(np.clip(2*(1-dist),0,None))
         return dist    
+
+class MVUnitEuclidean(object): 
+
+    def __init__(self,wlen:int,**kwargs)->None:
+        """Initialization
+
+        Args:
+            wlen (int): window length
+        """
+        self.wlen = wlen
+
+    def fit(self,signal:np.ndarray):
+        """Compute the elementary components of the first line of the crossdistance matrix. 
+
+        Args:
+            signal (np.ndarray): signal. shape (n_samples,n_dimension)
+        """
+        self.signal_ =  signal
+        self.ns_, self.nd_ = signal.shape
+        self.first_basic_ = self._first_elemntary()
+
+    def _first_elemntary(self)->np.ndarray:
+        """Compute the dotproduct between subsequence of the first line of the crossdistance matrix. 
+        Compute the mean and std for each subsequence. 
+        """
+        self.first_dot_product_ = fftconvolve(self.signal_[:self.wlen,:][::-1],self.signal_,'valid',axes=0)
+
+        means = np.zeros((self.ns_+1,self.nd_))
+        means[1:] = np.cumsum(self.signal_,axis=0)/self.wlen
+        self.means_ = means[self.wlen:]-means[:-self.wlen]
+
+        stds = np.zeros((self.ns_+1,self.nd_))
+        stds[1:] =np.cumsum(self.signal_**2,axis=0)/self.wlen
+        stds = stds[self.wlen:]-stds[:-self.wlen]
+        self.stds_ = np.sqrt(stds  - self.means_**2)
+
+    def first_line(self,i): 
+        """Compute the line of the crossdistance matrix at index i
+
+        Args:
+            i (int): index where start the computation of the chunk
+
+        Returns:
+            np.ndarray: crossditance matrix line at index i
+        """
+
+        self.first_idx_ = i
+        self.idx_ = i
+
+        if i!=0:
+            self.dot_product_ = fftconvolve(self.signal_[i:i+self.wlen][::-1],self.signal_,'valid',axes=0)
+        else: 
+            self.dot_product_ = self.first_dot_product_.copy()
+
+        dist = (self.dot_product_ - self.wlen*self.means_[i]*self.means_)/(self.wlen*self.stds_[i]*self.stds_)
+
+        return np.sqrt(np.clip(2*(1-dist),0,None).sum(axis=-1)/self.nd_)
+
+    def individual_distance(self,i,j): 
+        a = (self.signal_[i:i+self.wlen]-self.means_[i])/self.stds_[i]
+        b = (self.signal_[j:j+self.wlen]-self.means_[j])/self.stds_[j]
+        return np.sqrt(np.sum((a-b)**2))
+
+
+    def next_line(self)->np.ndarray:
+        """Iterator to compute the line of the crossdistance matrix
+
+        Returns:
+            np.ndarray: crossdistance matrix line
+        """
+        self.dot_product_[1:] = self.dot_product_[:-1] + \
+            self.signal_[self.idx_+self.wlen]*self.signal_[self.wlen:] - \
+                self.signal_[self.idx_]*self.signal_[:-self.wlen]
+        self.idx_ +=1 
+        self.dot_product_[0] = self.first_dot_product_[self.idx_]
+        dist = (self.dot_product_ - self.wlen*self.means_[self.idx_]*self.means_)/(self.wlen*self.stds_[self.idx_]*self.stds_)
+        dist = np.sqrt(np.clip(2*(1-dist),0,None).sum(axis=-1)/self.nd_)
+        return dist   
